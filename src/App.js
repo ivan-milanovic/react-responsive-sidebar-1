757efe62227b0b8b49bf60b5e3b5228755c91ddf
Ivan <ivan.milanovic87@gmail.com>
2020-10-10 15:05:55 +0200

    Navbar and sidebar done with react routes on pages

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Navbar from "./Components/Navbar";
+import{BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import Home from "./pages/Home";
+import Reports from "./pages/Reports";
+import Products from "./pages/Products";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <>
+           <Router>
+               <Navbar/>
+               <Switch>
+                   <Route path='/' exact component={Home} />
+                   <Route path='/reports' exact component={Reports} />
+                   <Route path='/products' exact component={Products}/>
+               </Switch>
+           </Router>
+        </>
+    );
 }
 
 export default App;
---
+++ src/pages/Reports.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Reports = () => {
+    return (
+        <>
+            <div className="reports">
+                <h1>Reports</h1>
+            </div>
+        </>
+    )
+}
+
+export default Reports;
